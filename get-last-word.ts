9392b2f94f66172edc784c37ad64d923ef9b36e5
ng-bilel <bilel.feki.92@gmail.com>
2025-04-29 07:48:49 +0100

    add getWordAtIndex,  divideWordWithLeftGreater and divideWordWithRightGreater

--- get-last-word.ts
+++ get-last-word.ts
@@ -1,3 +1,35 @@
 export function getLastWord(s: string): string {
   return s.trim().split(" ").pop() as string;
 }
+
+export function getWordAtIndex(s: string, index: number): string {
+  const sAsArray = s.trim().split(" ");
+  return sAsArray[index] ? sAsArray[index] : "";
+}
+
+export function divideWordWithLeftGreater(s: string): string[] {
+  const cleanString = s.trim();
+  if (cleanString.length == 0) {
+    return ["", ""];
+  }
+  return s.length % 2 == 0
+    ? [
+        cleanString.slice(0, cleanString.length / 2),
+        cleanString.slice(cleanString.length / 2),
+      ]
+    : [
+        cleanString.slice(0, cleanString.length / 2 + 1),
+        cleanString.slice((cleanString.length + 1) / 2),
+      ];
+}
+
+export function divideWordWithRightGreater(s: string): string[] {
+  const cleanString = s.trim();
+  if (cleanString.length == 0) {
+    return ["", ""];
+  }
+  return [
+    cleanString.slice(0, cleanString.length / 2),
+    cleanString.slice(cleanString.length / 2),
+  ];
+}
--- tests/last-string.test.ts
+++ tests/last-string.test.ts
@@ -1,6 +1,11 @@
-import { getLastWord } from "../get-last-word";
+import {
+  divideWordWithLeftGreater,
+  divideWordWithRightGreater,
+  getLastWord,
+  getWordAtIndex,
+} from "../get-last-word";
 
-describe("", () => {
+describe("getLastWord function ", () => {
   test("it should return a valid string when there are many words", () => {
     const inputString = "hello this a s first test case";
     expect(getLastWord(inputString)).toBe("case");
@@ -16,5 +21,64 @@ describe("", () => {
   test("it should return itself when single word provided ", () => {
     expect(getLastWord("")).toBe("");
     expect(getLastWord("hi")).toBe("hi");
+  });
+});
+describe("getWordAtIndex", () => {
+  test("it should return word at index 2", () => {
+    const inputString = "hello my name is bilel ";
+    expect(getWordAtIndex(inputString, 2)).toBe("name");
+  });
+  test("it should return empty string", () => {
+    const inputString = "  ";
+    expect(getWordAtIndex(inputString, 2)).toBe("");
+  });
+});
+
+describe("divideWordWithLeftGreater", () => {
+  test("it should divide the word at the middle", () => {
+    const inputString = "1122";
+    expect(divideWordWithLeftGreater(inputString)[0]).toBe("11");
+    expect(divideWordWithLeftGreater(inputString)[1]).toBe("22");
+  });
+  test("it should divide the word but keep the left side greater ", () => {
+    const inputString = "11233";
+    expect(divideWordWithLeftGreater(inputString)[0]).toBe("112");
+    expect(divideWordWithLeftGreater(inputString)[1]).toBe("33");
+  });
+
+  test("it should divide the word but keep the left side greater ", () => {
+    const inputString = " ";
+    expect(divideWordWithLeftGreater(inputString)[0]).toBe("");
+    expect(divideWordWithLeftGreater(inputString)[1]).toBe("");
+  });
+});
+
+describe("getWordAtIndex", () => {
+  test("it should return word at index 2", () => {
+    const inputString = "hello my name is bilel ";
+    expect(getWordAtIndex(inputString, 2)).toBe("name");
+  });
+  test("it should return empty string", () => {
+    const inputString = "  ";
+    expect(getWordAtIndex(inputString, 2)).toBe("");
+  });
+});
+
+describe("divideWordWithRightGreater", () => {
+  test("it should divide the word at the middle", () => {
+    const inputString = "1122";
+    expect(divideWordWithRightGreater(inputString)[0]).toBe("11");
+    expect(divideWordWithRightGreater(inputString)[1]).toBe("22");
+  });
+  test("it should divide the word but keep the right side greater ", () => {
+    const inputString = "11233";
+    expect(divideWordWithRightGreater(inputString)[0]).toBe("11");
+    expect(divideWordWithRightGreater(inputString)[1]).toBe("233");
+  });
+
+  test("it should return empty strings ", () => {
+    const inputString = " ";
+    expect(divideWordWithRightGreater(inputString)[0]).toBe("");
+    expect(divideWordWithRightGreater(inputString)[1]).toBe("");
   });
 });
